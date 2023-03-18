7bda9b81562c1d3a7d1e564bdd92da4322075c93
Jean-JacquesK <jj.kechichian@gmail.com>
2023-03-18 15:14:47 +0100

    Create app, and dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'>yo</div>;
+  return (
+    <div className='App'>
+      <h1>Movies</h1>
+    </div>
+  );
 }
 
 export default App;

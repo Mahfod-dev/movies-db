0ae3442b09bc9e31bf2b4cd3877d9d0d4c8e0abb
Jean-JacquesK <jj.kechichian@gmail.com>
2023-03-21 10:14:34 +0100

    footer + global css revue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import "./footer.css";
 
 function Footer() {
-  return <section className='footer-container'>Footer</section>;
+  return (
+    <section className='footer-container'>
+      <p>J'jack Kech development</p>
+    </section>
+  );
 }
 
 export default Footer;

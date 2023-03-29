3a03589dbf552986f0e1c56c07995d1350296e08
Mahfod <mahfod.dev@gmail.com>
2023-03-29 14:16:34 +0200

    change fetchapi

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,47 +1,53 @@
-import { useState } from "react";
-import { fetchTopRatedApi, fetchTrendingApi } from "../../utils/fetchApi";
-import Result from "../Result/Result";
-import SearchForm from "../Search/SearchForm";
+import { useState, useEffect } from 'react';
+import { fetchTopRatedApi, fetchTrendingApi } from '../../utils/fetchApi';
+import Result from '../Result/Result';
+import SearchForm from '../Search/SearchForm';
 
 function Main() {
-  const [trending, SetTrending] = useState([]);
-  const [searchMovie, setSearchMovie] = useState([]);
-  const [topRated, setTopRated] = useState([]);
+	const [searchMovie, setSearchMovie] = useState([]);
 
-  return (
-    <section className='main-container'>
-      <div className='search-container'>
-        <ul className='search-list'>
-          <li
-            className='search-item'
-            onClick={() => {
-              fetchTopRatedApi().then((res) => {
-                setTopRated(res);
-              });
-            }}
-          >
-            Top Rate Movies
-          </li>
-          <li
-            className='search-item search-active'
-            onClick={() => {
-              fetchTrendingApi().then((res) => {
-                SetTrending(res);
-              });
-            }}
-          >
-            Top Trend Movies
-          </li>
-        </ul>
-        <SearchForm setSearchMovie={setSearchMovie} />
-      </div>
-      <div className='result-container'>
-        {(topRated || trending || searchMovie).map((item) => {
-          return <Result key={item.id} item={item} />;
-        })}
-      </div>
-    </section>
-  );
+	useEffect(() => {
+		return () => {
+			fetchTopRatedApi().then((res) => {
+				setSearchMovie(res);
+			});
+		};
+	}, []);
+
+	return (
+		<section className='main-container'>
+			<div className='search-container'>
+				<ul className='search-list'>
+					<li
+						className='search-item'
+						onClick={() => {
+							fetchTopRatedApi().then((res) => {
+								setSearchMovie(res);
+							});
+						}}
+					>
+						Top Rate Movies
+					</li>
+					<li
+						className='search-item search-active'
+						onClick={() => {
+							fetchTrendingApi().then((res) => {
+								setSearchMovie(res);
+							});
+						}}
+					>
+						Top Trend Movies
+					</li>
+				</ul>
+				<SearchForm setSearchMovie={setSearchMovie} />
+			</div>
+			<div className='result-container'>
+				{searchMovie.map((item) => {
+					return <Result key={item.id} item={item} />;
+				})}
+			</div>
+		</section>
+	);
 }
 
 export default Main;
